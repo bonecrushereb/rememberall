5db4fadedabf04246e491807381c1c15a2882a6e
bonecrushereb <bonecrushereb@gmail.com>
2017-02-02 11:34:31 -0800

    fixing errors

--- scripts/app.js
+++ scripts/app.js
@@ -19,5 +19,33 @@ function add() {
   return false;
 }
 
+function remove() {
+  var id = this.getAttribute('id');
+  var todos = get_todos();
+  todos.splice(id, 1);
+  localStorage.setItem('todo', JSON.stringify(todos));
+
+  show();
+
+  return false;
+}
+
+function show() {
+  var todos = get_todos();
+
+  var html = '<ul>';
+  for (var i = 0; i < todos.length; i++) {
+    html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+  };
+  html += '</ul>';
+
+  document.getElementById('todos').innerHtml = html;
+
+  var buttons = document.getElementsByClassName('remove');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', remove);
+  };
+}
+
 document.getElementById('add').addEventListener('click', add);
 show();
