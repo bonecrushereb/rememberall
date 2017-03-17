404873a274e7e2ac81ee228afeae206fd2f804fb
bonecrushereb <bonecrushereb@gmail.com>
2017-03-17 10:50:00 -0700

    revising for objects

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,36 @@
-function ConvertFormToJSON(form) {
-  var array = $(form).serializeArray();
-  var json = {};
 
-  $.each(array, function() {
-    json[this.name] = this.value || '';
-  });
-
-  return json;
-}
-
-jQuery(document).on('ready', function() {
-  jQuery('form#add-new-task').bind('submit', function(event){
-    event.preventDefault();
-
-    var form = this;
-    var json = ConvertFormToJSON(form);
-    var tbody = jQuery('#to-do-list > tbody');
-
-    $.ajax({
-      type: "POST",
-      url: "submit.php",
-      data: json,
-      dataType: "json"
-    }).success(function(state) {
-      if(state.success === true) {
-        tbody.append('<tr><th scope="row" style="background-color:' + state['color'] +
-          '"><input type="checkbox" /></th><td>' + state['date'] +
-          '</td><td>' + state['priority'] + '</td><td>' + state['name'] +
-          '</td><td>' + state['desc'] + '</td><td>' + state['email'] + '</td></tr>');
-      } else {
-        alert(state.error.join());
-      }
-    }).fail(function(state) {
-      alert("Failed to add to-do");
-    });
-
-    return true;
+$(function() {
+  $("#datepicker").datepicker({
+    showOtherMonths: true,
+    selectOtherMonths: true,
   });
 });
+
+function get_todos() {
+  var todos_str = JSON.localStorage.getItem('allTodos');
+  if (todos_str != null) {
+    todos = JSON.parse(todos_str);
+  }
+  return todos;
+}
+
+function add() {
+  console.log("hello from add");
+  var allTodos = [];
+  var task = document.getElementById('task').value;
+  var prior = document.getElementById('priority').value;
+  var date = document.getElementById('datepicker').value;
+
+  var task = {
+    "task": task,
+    "priority": priority,
+    "date": date
+  };
+  localStorage.setItem('todo', JSON.stringify(todo));
+  allTodos.push(task);
+  localStorage.setItem('allTodos', JSON.stringify(allTodos));
+
+  return false;
+}
+
+document.getElementById('add').addEventListener("click", add);
